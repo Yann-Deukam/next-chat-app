0d4157febd522144dbe39e3363a2e27ff61a19e8
Yann-Donald <yanndonald237@gmail.com>
2024-11-30 10:56:12 +0100

    Setting functionality

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -21,22 +21,24 @@ export default function Button({
   disabled,
 }: ButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      type={type}
-      disabled={disabled}
-      className={clsx(
-        `flex justify-center px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
-        disabled && "opacity-50 cursor-default",
-        fullWidth && "w-full",
-        secondary ? "text-zinc-900" : "text-white",
-        danger && "bg-red-500 hover:bg-red-600 focus-visible:outline-red-600",
-        !secondary &&
-          !danger &&
-          "bg-purple-500 hover:bg-purple-600 focus-visible:outline-purple-600"
-      )}
-    >
-      {children}
-    </button>
+    <div>
+      <button
+        onClick={onClick}
+        type={type}
+        disabled={disabled}
+        className={clsx(
+          `flex justify-center px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
+          disabled && "opacity-50 cursor-default",
+          fullWidth && "w-full",
+          secondary ? "text-zinc-900" : "text-white",
+          danger && "bg-red-500 hover:bg-red-600 focus-visible:outline-red-600",
+          !secondary &&
+            !danger &&
+            "bg-purple-500 hover:bg-purple-600 focus-visible:outline-purple-600"
+        )}
+      >
+        {children}
+      </button>
+    </div>
   );
 }
